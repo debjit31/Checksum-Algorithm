a01c58d5a0d4dc924d25c876e6ed754785de9ff5
Debjit Chattopadhyay <37740164+debjit31@users.noreply.github.com>
2019-03-27 01:01:31 +0530

    Add files via upload

--- checksum.py
+++ checksum.py
@@ -1,3 +1,7 @@
+from operator import xor
+import sys
+import random
+flag = 0
 def count(n):
     ### Counting the number of digits for  binary representation of a deciml number
     digits = 0
@@ -20,35 +24,63 @@ def sender():
         nob = count(d)
         ## calculating the complemented sum for each data item
         ## we perform a XOR operation between the the data and 2^n - 1 
-        sum_s += ((pow(2,nob)-1)^d)
+        sum_s += xor((pow(2,nob)-1) , d)
     ## Appending the complementd sum in the data set 
     data.append(sum_s)
     print("Data set to be transmitted : -",data)
     print("Sender Checksum :-",sum_s)
     print("\n")
-    receiver(data)
-    
+    tm=int(input("Choose transmissiom mode.\n1.Error-Prone.\n2.Error-free.\nEnter your choice : - "))   
+    if tm == 1:
+        error_generator(data)
+    else:
+        receiver(data)
+                
 def receiver(data):
     print("Welcome to Receiver's End")
     sum_r = 0
+    print("Dataset received by receiver is  = ",data)
     for i in range(len(data)):
-        ## Calculating the no. of digits required to  represent a data in binary
+        ## Read each data item calculating the no. of digits required to  represent a data in binary
         nob = count(data[i])
+        d = data[i]
         #Calculating the complemented sum for the data set
-        sum_r += ((1<<nob)-1)^data[i]
+        sum_r += xor((pow(2,nob)-1) , d)
     print("1's Complemented Receiver Sum = ",sum_r)
     ## No. of binary digits for the 1;s complemented sum
     nob = count(sum_r)
     ## Complementing the Sum
-    sum_r = ((1<<nob)-1)^sum_r
+    sum_r = xor(((1<<nob)-1), sum_r)
     print("Receiver Checksum :- ",sum_r)
     data_checker(sum_r)
-
+    
+def error_generator(data):
+    for i in range(random.randint(0,5)):
+        #generating a random position from the list
+        rp= random.randint(-1,len(data)-1)
+        #print(data[rp])
+        #generating a random offset value to multiply with the original data
+        offset = random.randint(1,10)
+        data[rp] += offset 
+    receiver(data)
+    
 def data_checker(sum_r):
     ## if the complemented sum is 0 , the data is correct
     if sum_r == 0:
         print("Transmission Successfull!!!!")
     else:
         print("Error in Transmission")    
-sender()
+        flag = 1
+        program_runner()
 
+def program_runner():
+    print("Welcome to Checksum Error Detection Algorithm\n")
+    while True:
+        ch = input("Want to tranmit some data to  your neighbour ? Press y or n: ")
+        if ch == 'y' or ch == 'Y':
+            sender()
+        elif ch == 'n' or ch == 'N':
+            print("Goodbye!!!")
+            break
+    quit()
+program_runner()
